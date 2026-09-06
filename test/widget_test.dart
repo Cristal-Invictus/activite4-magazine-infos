e0ff7b635cc5948df8afab1e1797aa00e1043d93
Cristal AKOYESSOU <cristal.akoyessou@epitech.eu>
2026-09-06 22:20:05 +0100

    Activité 4.2 - Mettre à jour les tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:activite1/main.dart';
 
 void main() {
-  testWidgets('Magazine Infos est affiché', (WidgetTester tester) async {
+  testWidgets('Les éléments principaux sont affichés', (WidgetTester tester) async {
     await tester.pumpWidget(const MonAppli());
     expect(find.text('Magazine Infos'), findsOneWidget);
+    expect(find.text('Bienvenue au Magazine Infos'), findsOneWidget);
+    expect(find.text('TEL'), findsOneWidget);
+    expect(find.text('MAIL'), findsOneWidget);
+    expect(find.text('PARTAGE'), findsOneWidget);
   });
 }
